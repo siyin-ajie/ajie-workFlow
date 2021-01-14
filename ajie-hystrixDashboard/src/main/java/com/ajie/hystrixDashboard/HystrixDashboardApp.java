176e23f98bf124d765756ddd75f27379b8f3581e
jie <gao_hongjie@vgrass.com>
2021-01-14 15:46:36 +0800

    fix(*):修改熔断器可视化项目名称

--- ajie-hystrixDashboard/src/main/java/com/ajie/hystrixDashboard/HystrixDashboardApp.java
+++ ajie-hystrixDashboard/src/main/java/com/ajie/hystrixDashboard/HystrixDashboardApp.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
- * @name Hystrix熔断器监控可视化
+ * @name 熔断器监控可视化
  * 
  * @author hongjie.gao
- * @date 2021年1月14日下午3:12:32
+ * @date 2021年1月14日下午3:38:42
  */
 @SpringBootApplication
 @EnableHystrixDashboard
